68219febe81fcf88e6a0bb3ace93af8b194ba775
nisinsoba <soba.nisin@gmail.com>
2025-02-16 21:06:28 +0900

    add euclidean

---
+++ euclidean.py
@@ -0,0 +1,15 @@
+
+def euclid(a: int, b: int):
+    while b:
+        a, b = b, a % b
+    if a < 0: 
+        return -a
+    return a
+
+def main():
+    gcd = euclid(-10,-5)
+    print(gcd)
+
+if __name__ == "__main__":
+    main()
+
---
+++ ex_euclidean.py
@@ -0,0 +1,24 @@
+
+def ex_euc(a: int, b: int):
+    x, xx, y, yy = 0, 1, 1, 0
+    if b == 0: 
+        x, y, b = 1, 0, a
+    else:
+        while a % b:
+            q, r = a // b, a % b
+            tmpx, tmpy = xx - q*x, yy - q*y
+            xx, yy = x, y
+            x, y = tmpx, tmpy
+            a, b = b, r
+    if b < 0: 
+        return -x, -y, -b
+    return x, y, b
+
+def main():
+    a, b = -3, -10
+    x, y, gcd = ex_euc(a, b)
+    print(f"{a}*{x} + {b}*{y} = {gcd}")
+
+if __name__ == "__main__":
+    main()
+
